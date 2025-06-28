38945dbfc974f767da07eb48d5a406e90c0827b0
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-28 11:11:38 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -18,7 +18,7 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 API_ID = "27765349" #config("API_ID", default=None, cast=int)
 API_HASH = "9df1f705c8047ac0d723b29069a1332b" #config("API_HASH", default=None)
 BOT_TOKEN = "7779351596:AAFuSwUpssu4Nt9kf-yk_sIJu1trPudmShI" #config("BOT_TOKEN", default=None)
-SESSION = "BQHDMOUAIOGZHesmwkhKztZ1bU7NokB1HVLtNKHAnr35ElBp-FQ7IPkvayF0s5JoOGLN44ksi4kqeUNxnG56Vd8Mh_2Lo3ICHSN2J2u0WyYIOj96FBxN2gq_iekABQkL-vdXTB1DrOswqzBJBG9RaGPFVoiEYDAd0iD2vqgT3x2wOz98gBZNKPCGpWQYbGR6GKe66W5SRZRlLWJaEDQcTEIxNF48nIEGW7cwK2AG3eR4-iyVg5Zxaje_ACeNuCN5kLtQsNkGEV23f7-EdLQTG1zKnZ57AjUvYQdJ7o1pdGhkKknUUmOcfG4xn42RbHUwccqD1CmsGLU5Zh-vTbgBGh9AiP79HAAAAAGlHSMFAA" #config("SESSION", default=None)
+SESSION = "" config("SESSION", default=None)
 FORCESUB = "checking123456789" #config("FORCESUB", default=None)
 AUTH = "1116405290" #config("AUTH", default=None)
 SUDO_USERS = [1116405290]
